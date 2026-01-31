70df148a320ec6e5d5c84307ab6d6ce5d4c01dd2
QuinnJack <108008843+QuinnJack@users.noreply.github.com>
2026-01-31 17:42:18 -0500

    second iteration, add images, styles for product display and cart, accessiblity and styles

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -4,68 +4,144 @@
 
 var products = [
 	{
-		name: "brocoli",
+		name: "broccoli",
 		vegetarian: true,
 		glutenFree: true,
+		lactoseFree: true,
 		organic: true,
-		price: 1.99
+		price: 1.99,
+		image: "https://cdn.britannica.com/25/78225-050-1781F6B7/broccoli-florets.jpg"
 	},
 	{
-		name: "bread",
+		name: "wheat bread",
 		vegetarian: true,
 		glutenFree: false,
+		lactoseFree: true,
 		price: 2.35,
-		organic: false
+		organic: false,
+		image: "https://thumbs.dreamstime.com/b/fresh-square-slice-bread-made-white-wheat-flour-isolated-background-186186771.jpg"
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
+		lactoseFree: true,
 		price: 10.00,
-		organic: true
+		organic: true,
+		image: "https://gdimg.gmarket.co.kr/1977401587/still/400?ver=1768022396"
 	},
 	{
 		name: "pasta",
 		vegetarian: true,
 		glutenFree: false,
+		lactoseFree: true,
 		price: 3.50,
-		organic: false
+		organic: false,
+		image: "https://digital.loblaws.ca/PCX/20081342001_EA/en/1/20081342001_en_front_400.png"
 	},
 	{
 		name: "tofu",
 		vegetarian: true,
 		glutenFree: true,
+		lactoseFree: true,
 		price: 4.25,
-		organic: true
+		organic: true,
+		image: "https://product-images.metro.ca/images/h7b/h1d/15974929268766.jpg"
+	},
+	{
+		name: "milk",
+		vegetarian: true,
+		glutenFree: true,
+		lactoseFree: false,
+		price: 3.10,
+		organic: false,
+		image: "https://g-mart.ca/wp-content/uploads/2020/10/large_2f483bf3-bde7-47d8-9d3b-709ec6c84292-600x600.jpeg"
+	},
+	{
+		name: "lactose-free milk",
+		vegetarian: true,
+		glutenFree: true,
+		lactoseFree: true,
+		price: 3.80,
+		organic: false,
+		image: "https://media.sobeys.com/original/754000000025700000000035003880_VOILA_a6d79b735430f152a0b94c942ddd17b91bc7668e.JPG"
+	},
+	{
+		name: "cheddar cheese",
+		vegetarian: true,
+		glutenFree: true,
+		lactoseFree: false,
+		price: 5.40,
+		organic: false,
+		image: "https://images.cdn.saveonfoods.com/detail/00058898540030.jpg"
+	},
+	{
+		name: "eggs",
+		vegetarian: true,
+		glutenFree: true,
+		lactoseFree: true,
+		price: 2.75,
+		organic: true,
+		image: "https://thumbs.dreamstime.com/b/carton-box-white-eggs-isolated-png-transparent-269959810.jpg"
+	},
+	{
+		name: "apples",
+		vegetarian: true,
+		glutenFree: true,
+		lactoseFree: true,
+		price: 1.50,
+		organic: false,
+		image: "https://hips.hearstapps.com/hmg-prod/images/red-fresh-apple-isolated-on-white-background-royalty-free-image-1627314996.jpg?crop=1.00xw:0.923xh;0,0.0486xh"
+	},
+	{
+		name: "almonds",
+		vegetarian: true,
+		glutenFree: true,
+		lactoseFree: true,
+		price: 6.20,
+		organic: true,
+		image: "https://digital.loblaws.ca/PCX/20500386003_EA/en/1/20500386003_en_front_800.png"
 	}
 ]
-//TODO: Sort products by price
-function restrictListProducts(prods, restriction) {
-	let product_names = [];
-		for (let i=0; i<prods.length; i+=1) {
-			if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-				product_names.push(prods[i].name);
-			}
-			else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-				product_names.push(prods[i].name);
-			}
-			else if ((restriction == "LactoseFree") && (prods[i].glutenFree == true)){
-				product_names.push(prods[i].name);
-			}
-			else if (restriction == "None"){
-				product_names.push(prods[i].name);
-			}
+// Return a filtered list of products sorted by price (ascending)
+function restrictListProducts(prods, preferences) {
+	let filtered = [];
+	for (let i = 0; i < prods.length; i += 1) {
+		let include = true;
+		if (preferences.vegetarian && !prods[i].vegetarian) {
+			include = false;
 		}
-	
-	return product_names;
+		if (preferences.glutenFree && !prods[i].glutenFree) {
+			include = false;
+		}
+		if (preferences.lactoseFree && !prods[i].lactoseFree) {
+			include = false;
+		}
+		if (preferences.organicPreference === "Organic" && !prods[i].organic) {
+			include = false;
+		}
+		if (preferences.organicPreference === "Non-Organic" && prods[i].organic) {
+			include = false;
+		}
+		if (include) {
+			filtered.push(prods[i]);
+		}
+	}
+	filtered.sort(function(a, b) {
+		return a.price - b.price;
+	});
+	return filtered;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
-	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -1){
-			totalPrice += products[i].price;
+	for (let i = 0; i < chosenProducts.length; i += 1) {
+		for (let j = 0; j < products.length; j += 1) {
+			if (chosenProducts[i].name === products[j].name) {
+				totalPrice += products[j].price * chosenProducts[i].qty;
+				break;
+			}
 		}
 	}
 	return totalPrice;
--- scripts/main.js
+++ scripts/main.js
@@ -24,15 +24,20 @@ function openInfo(evt, tabName) {
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
+function populateListProductChoices() {
+    var s2 = document.getElementById("displayProduct");
 	
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
     s2.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+    var preferences = {
+		vegetarian: document.getElementById("dietVegetarian").checked,
+		glutenFree: document.getElementById("dietGlutenFree").checked,
+		lactoseFree: document.getElementById("dietLactoseFree").checked,
+		organicPreference: document.getElementById("foodPreference").value
+	};
+    var optionArray = restrictListProducts(products, preferences);
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
@@ -40,22 +45,63 @@ function populateListProductChoices(slct1, slct2) {
 		
 	for (i = 0; i < optionArray.length; i++) {
 			
-		var productName = optionArray[i];
+		var productName = optionArray[i].name;
+		var productPrice = optionArray[i].price.toFixed(2);
+		var productOrganic = optionArray[i].organic ? "Organic" : "Non-organic";
+		var productId = productName.toLowerCase().replace(/\s+/g, "-").replace(/[^a-z0-9-_]/g, "");
+
+		var itemRow = document.createElement("div");
+		itemRow.className = "product-item";
+
+		if (optionArray[i].image) {
+			var img = document.createElement("img");
+			img.src = optionArray[i].image;
+			img.alt = productName;
+			img.className = "product-image";
+			itemRow.appendChild(img);
+		} else {
+			var imgPlaceholder = document.createElement("div");
+			imgPlaceholder.className = "product-image placeholder";
+			imgPlaceholder.appendChild(document.createTextNode("IMG"));
+			itemRow.appendChild(imgPlaceholder);
+		}
+
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.name = "product";
 		checkbox.value = productName;
-		s2.appendChild(checkbox);
+		checkbox.id = "check-" + productId;
+		itemRow.appendChild(checkbox);
 		
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
-		label.htmlFor = productName;
+		label.htmlFor = checkbox.id;
+		label.className = "product-label";
 		label.appendChild(document.createTextNode(productName));
-		s2.appendChild(label);
+		itemRow.appendChild(label);
+
+		var price = document.createElement("span");
+		price.className = "product-price";
+		price.appendChild(document.createTextNode("$" + productPrice + " (" + productOrganic + ")"));
+		itemRow.appendChild(price);
+
+		var qtyLabel = document.createElement("label");
+		qtyLabel.htmlFor = "qty-" + productId;
+		qtyLabel.className = "product-qty-label";
+		qtyLabel.appendChild(document.createTextNode("Qty"));
+		itemRow.appendChild(qtyLabel);
+
+		var qtyInput = document.createElement("input");
+		qtyInput.type = "number";
+		qtyInput.min = "1";
+		qtyInput.value = "1";
+		qtyInput.id = "qty-" + productId;
+		qtyInput.className = "product-qty";
+		qtyInput.setAttribute("data-name", productName);
+		itemRow.appendChild(qtyInput);
 		
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
+		s2.appendChild(itemRow);
 	}
 }
 
@@ -70,21 +116,124 @@ function selectedItems(){
 	var c = document.getElementById('displayCart');
 	c.innerHTML = "";
 	
+	var cartList = document.createElement("div");
+	cartList.className = "product-list";
+
 	// build list of selected item
 	var para = document.createElement("P");
 	para.innerHTML = "You selected : ";
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) { 
 		if (ele[i].checked) {
-			para.appendChild(document.createTextNode(ele[i].value));
-			para.appendChild(document.createElement("br"));
-			chosenProducts.push(ele[i].value);
+			var name = ele[i].value;
+			var productId = name.toLowerCase().replace(/\s+/g, "-").replace(/[^a-z0-9-_]/g, "");
+			var qtyInput = document.getElementById("qty-" + productId);
+			var qty = parseInt(qtyInput.value, 10);
+			if (!qty || qty < 1) {
+				qty = 1;
+				qtyInput.value = "1";
+			}
+			var priceEach = 0;
+			var productImage = "";
+			for (let j = 0; j < products.length; j += 1) {
+				if (products[j].name === name) {
+					priceEach = products[j].price;
+					productImage = products[j].image;
+					break;
+				}
+			}
+			var lineTotal = (priceEach * qty).toFixed(2);
+
+			var cartRow = document.createElement("div");
+			cartRow.className = "product-item cart-item";
+
+			if (productImage) {
+				var cartImg = document.createElement("img");
+				cartImg.src = productImage;
+				cartImg.alt = name;
+				cartImg.className = "product-image";
+				cartRow.appendChild(cartImg);
+			} else {
+				var cartImgPlaceholder = document.createElement("div");
+				cartImgPlaceholder.className = "product-image placeholder";
+				cartImgPlaceholder.appendChild(document.createTextNode("IMG"));
+				cartRow.appendChild(cartImgPlaceholder);
+			}
+
+			var nameSpan = document.createElement("span");
+			nameSpan.className = "cart-name";
+			nameSpan.appendChild(document.createTextNode(name));
+			cartRow.appendChild(nameSpan);
+
+			var qtySpan = document.createElement("span");
+			qtySpan.className = "cart-qty";
+			qtySpan.appendChild(document.createTextNode("x" + qty));
+			cartRow.appendChild(qtySpan);
+
+			var unitSpan = document.createElement("span");
+			unitSpan.className = "cart-unit";
+			unitSpan.appendChild(document.createTextNode("$" + priceEach.toFixed(2)));
+			cartRow.appendChild(unitSpan);
+
+			var totalSpan = document.createElement("span");
+			totalSpan.className = "cart-line-total";
+			totalSpan.appendChild(document.createTextNode("$" + lineTotal));
+			cartRow.appendChild(totalSpan);
+
+			var removeBtn = document.createElement("button");
+			removeBtn.type = "button";
+			removeBtn.className = "cart-remove";
+			removeBtn.setAttribute("aria-label", "Remove " + name);
+			removeBtn.appendChild(document.createTextNode("X"));
+			removeBtn.onclick = function() {
+				for (let k = 0; k < ele.length; k += 1) {
+					if (ele[k].value === name) {
+						ele[k].checked = false;
+						break;
+					}
+				}
+				selectedItems();
+			};
+			cartRow.appendChild(removeBtn);
+
+			if (qty === 1) {
+				qtySpan.style.visibility = "hidden";
+				unitSpan.style.visibility = "hidden";
+			}
+
+			cartList.appendChild(cartRow);
+			chosenProducts.push({ name: name, qty: qty });
 		}
 	}
-		
+	if (chosenProducts.length === 0) {
+		c.appendChild(document.createTextNode("Your cart is empty."));
+		return;
+	}
+	
 	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	c.appendChild(cartList);
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts).toFixed(2)));
 		
 }
 
+window.onload = function() {
+	populateListProductChoices();
+	var c = document.getElementById('displayCart');
+	if (c) {
+		c.innerHTML = "Your cart is empty.";
+	}
+};
+
+function updateFontSize(sizePx) {
+	var size = parseInt(sizePx, 10);
+	if (!size) {
+		return;
+	}
+	document.documentElement.style.fontSize = size + "px";
+	document.body.style.fontSize = size + "px";
+	var display = document.getElementById("fontSizeValue");
+	if (display) {
+		display.textContent = size + "px";
+	}
+}
